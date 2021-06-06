572cb7abb0f578fa0121b4ade8fe66e13c1bb34a
Arturo <gwozdyk@gmail.com>
2021-06-06 10:10:06 +0200

    energy_bar

--- lib/life.py
+++ lib/life.py
@@ -13,6 +13,7 @@ class Life(Body):
 
     def __init__(self, screen: Surface, space: Space, world_size: Vec2d, size: int, color0: Color, color1: Color, color2: Color, angle: float=None, visual_range: int=180, position: Vec2d=None):
         super().__init__(self, body_type=Body.KINEMATIC)
+        self.enemy = 1000
         self.screen = screen
         self.color0 = color0
         self.color1 = color1
@@ -46,6 +47,7 @@ class Life(Body):
         r = self.shape.radius
         rot = self.rotation_vector
         #self.draw_detectors()
+        self.draw_energy_bar(int(x), flipy(int(y)))
         gfxdraw.filled_circle(self.screen, int(x), flipy(int(y)), int(r), self.color0)
         gfxdraw.filled_circle(self.screen, int(x), flipy(int(y)), int(r-2), self.color1)
         if r > 2:
@@ -62,7 +64,9 @@ class Life(Body):
     def update(self, space: Space, dt:float, detections: list=[]) -> None:
         #self.update_detections(detections)
         self.analize()
-        self.random_move(space, dt)
+        move_energy = self.random_move(space, dt)
+        base_energy = 1/dt
+        self.energy -= (move_energy + base_energy)
 
     def update_detections(self, detections: list):        
         for detector in self.sensors:
@@ -82,6 +86,7 @@ class Life(Body):
         self.velocity = move*self.vdir/dt
         self.sensors[1].rotate(senor_turn, 0, PI/1.5)
         self.sensors[2].rotate(-senor_turn, -PI/1.5, 0)
+        return move/dt
 
     def analize(self):
         input = []
@@ -96,3 +101,8 @@ class Life(Body):
             if e:
                 print(f'INPUT detected: {d} | distance: {d} | angle: {a}')
        
+    def draw_energy_bar(self, rx: int, ry: int):
+        bar_red = Color(255, 0, 0)
+        bar_green = Color(0, 255, 0)
+        gfxdraw.box(self.screen, Rect(rx-round(10), ry+round((self.size+3)), round(19), 1), bar_red)
+        gfxdraw.box(self.screen, Rect(rx-round(10), ry+round((self.size+3)), round(20*(self.energy/1000)), 1), bar_green)
