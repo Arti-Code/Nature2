2847ff476e102f0adb6a4eab2aa50d1ce9ebae42
Arturo <41492101+Arturr0@users.noreply.github.com>
2022-04-19 18:54:47 +0200

    v0.8.52

--- lib/creature.py
+++ lib/creature.py
@@ -1,4 +1,5 @@
 from copy import copy, deepcopy
+#from nis import match
 from random import random, randint
 from math import log, sin, cos, radians, degrees, floor, ceil, pi as PI, sqrt
 from statistics import mean
@@ -19,6 +20,10 @@ from lib.camera import Camera
 from lib.vision import Vision
 
 class Creature(Life):
+
+    ATTACK_EYES: Color=Color('red')
+    EAT_EYES: Color=Color('yellow')
+    NORMAL_EYES: Color=Color('skyblue')
 
     def __init__(self, screen: Surface, space: Space, time: int, collision_tag: int, position: Vec2d, genome: dict=None, color0: Color=Color('grey'), color1: Color=Color('skyblue'), color2: Color=Color('orange'), color3: Color=Color('red')):
         super().__init__(screen=screen, space=space, collision_tag=collision_tag, position=position)
@@ -192,7 +197,12 @@ class Creature(Life):
             gfxdraw.aacircle(screen, int(rx), int(ry), int(r2), Color(r, g, b, a))
             gfxdraw.filled_circle(screen, int(rx), int(ry), int(r2), Color(r, g, b, a))
             gfxdraw.filled_circle(screen, int(x3), int(y3), int(r2*0.67), Color('black'))
-        self.vision.draw(screen=screen, camera=camera, rel_position=rel_pos, selected=marked)
+        eyes_color: Color=self.NORMAL_EYES
+        if self.attacking:
+            eyes_color=self.ATTACK_EYES
+        elif self.eating:
+            eyes_color=self.EAT_EYES
+        self.vision.draw(screen=screen, camera=camera, rel_position=rel_pos, selected=marked, eye_color=eyes_color)
         self.color0 = self._color0
         self.draw_energy_bar(screen, rx, ry)
         #self.reset_collisions()
