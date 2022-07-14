dd6c0e338e949f012b408a92377682afa0785e9a
Arturo <41492101+Arturr0@users.noreply.github.com>
2022-07-14 20:38:44 +0200

    v0.9.13

--- lib/config.py
+++ lib/config.py
@@ -2,7 +2,7 @@ from math import pi as PI
 from json import loads, dumps
 
 TITLE = "NATURE"
-SUBTITLE = "v0.9.12" 
+SUBTITLE = "v0.9.13" 
 AUTHOR = "2019-2022 Artur Gwoździowski"
 
 class Configuration():
