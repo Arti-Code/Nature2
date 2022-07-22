e6af468b8f691f57a1a2b10df28b6246da01ca45
Arturo <41492101+Arturr0@users.noreply.github.com>
2022-07-22 09:33:53 +0200

    timer

--- lib/utils.py
+++ lib/utils.py
@@ -10,3 +10,41 @@ class Detection(IntEnum):
     PLANT = 2
     MEAT = 3
     ROCK = 4
+
+class Timer():
+
+    def __init__(self, interval: float, one_shoot: bool=True, autostart: bool=False, label: str=None):
+        self.time = 0
+        self.run: bool=autostart
+        self.interval = interval
+        self.one_shoot = one_shoot
+        self.label = label
+
+    def timeout(self, delta: float) -> bool:
+        if not self.run:
+            return False
+        self.time += delta
+        if self.time < self.interval:
+            return False
+        self.time = 0
+        if self.one_shoot:
+            self.run = False
+        return True
+
+    def stop(self):
+        self.run = False
+
+    def start(self):
+        self.run = True
+
+    def restart(self):
+        self.time = 0
+        self.run = True
+
+    def set_timer(self, interval: float, one_shoot: bool=True):
+        self.time = 0
+        self.run: True
+        self.interval = interval
+        self.one_shoot = one_shoot
+        
+
