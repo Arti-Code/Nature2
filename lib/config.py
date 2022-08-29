5783359bc53035899f184ba18b4f2b9720736e92
Arturo <41492101+Arturr0@users.noreply.github.com>
2022-08-29 07:46:50 +0200

    cfg

--- lib/config.py
+++ lib/config.py
@@ -2,7 +2,7 @@ from math import pi as PI
 from json import loads, dumps
 
 TITLE = "NATURE"
-SUBTITLE = "v1.1.0 (ZoomIn/ZoomOut)" 
+SUBTITLE = "v1.1.1" 
 AUTHOR = "2019-2022 Artur Gwoździowski"
 
 class Configuration():
@@ -11,6 +11,7 @@ class Configuration():
         self.WORLD = None
         self.SCREEN = None
         self.ITER = None
+
         self.PLANT_MAX_SIZE = None
         self.PLANT_GROWTH = None
         self.PLANT_INIT_NUM = None
@@ -19,29 +20,36 @@ class Configuration():
         self.PLANT_LIFE = None
         self.PLANT_RANGE = None
         self.PLANT_EDGE = None
+
         self.CREATURE_MULTIPLY = None
         self.CREATURE_MIN_NUM = None
         self.CREATURE_INIT_NUM = None
-        self.CREATURES_SEP = None
-        self.EAT = None
-        self.BASE_ENERGY = None
-        self.MOVE_ENERGY = None
-        self.REP_TIME = None
-        self.REP_ENERGY = None
-        self.SPEED = None
-        self.TURN = None
         self.CREATURE_MIN_SIZE = None
         self.CREATURE_MAX_SIZE = None
+        self.CREATURES_SEP = None
+
+        self.BASE_ENERGY = None
+        self.MOVE_ENERGY = None
+        self.REP_ENERGY = None
+
+        self.EAT = None
+        self.SPEED = None
+        self.TURN = None
         self.HIT = None
+
+        self.REP_TIME = None
         self.MEM_TIME = None
+        self.MEAT_TIME = None
+
         self.SENSOR_MAX_ANGLE = None
         self.CLOSE_VISION = None
+
         self.ROCK_NUM = None
         self.ROCK_SIZE_MIN = None
         self.ROCK_SIZE_MAX = None
         self.ROCK_VERT_MIN = None
+
         self.RANK_SIZE = None
-        self.MEAT_TIME = None
         self.SIZE2ENG = None
         self.SIZE_COST = None
         self.POWER_COST = None
