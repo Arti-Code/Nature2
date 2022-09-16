64ebb399b55042cc19053a171d20b2b659f9bd86
Arturo <41492101+Arturr0@users.noreply.github.com>
2022-09-16 03:12:28 +0200

    dng

--- lib/creature.py
+++ lib/creature.py
@@ -335,8 +335,10 @@ class Creature(Life):
         input = []
         ar, ad, af, aw, pr, pd, mr, md, rr, rd = self.vision.get_detection2()
         eng = self.energy/self.max_energy
-        pwr = self.size+self.power+(self.attacking*10)
-        dng = clamp((aw-pwr)/30, -1, 1)
+        dng = 0.0
+        if aw != 0:
+            pwr = self.size+self.power+(self.attacking*10)
+            dng = clamp((aw-pwr)/30, -1, 1)
         input.append(self.collide_creature)
         input.append(self.collide_plant)
         input.append(self.collide_meat)
