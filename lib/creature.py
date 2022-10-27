0e4a4c7f5722b231f6d5778f9c17b8e34b49ffb5
Arturo <41492101+Arturr0@users.noreply.github.com>
2022-10-27 16:05:50 +0200

    x

--- lib/creature.py
+++ lib/creature.py
@@ -344,8 +344,14 @@ class Creature(Life):
             rest_energy += cfg.ATK_ENG
         base_energy *= size_cost
         total_eng_cost = (base_energy + move_energy + rest_energy + neuro_energy)
-        self.eng_lost = {'basic': base_energy, 'move': move_energy, 'neuro': neuro_energy, 'other': rest_energy}
-        self.energy -= total_eng_cost * dt
+        if self.hidding:
+            self.eng_lost = {'basic': base_energy*0.5, 'move': move_energy*0.5, 'neuro': neuro_energy*0.5, 'other': rest_energy*0.5}
+        else:
+            self.eng_lost = {'basic': base_energy, 'move': move_energy, 'neuro': neuro_energy, 'other': rest_energy}
+        if self.hidding:
+            self.energy -= total_eng_cost * dt * 0.5
+        else:
+            self.energy -= total_eng_cost * dt
         self.energy = clamp(self.energy, 0, self.max_energy)
 
     def get_input(self):
